d4b5d6ab17c3844132e662f52f7803cab72ca17e
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 16:38:22 +0100

    Initial Commit

---
+++ InAppPurchase/InAppPurchase.h
@@ -0,0 +1,19 @@
+//
+//  InAppPurchase.h
+//  InAppPurchase
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppPurchase.
+FOUNDATION_EXPORT double InAppPurchaseVersionNumber;
+
+//! Project version string for InAppPurchase.
+FOUNDATION_EXPORT const unsigned char InAppPurchaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppPurchase/PublicHeader.h>
+
+
